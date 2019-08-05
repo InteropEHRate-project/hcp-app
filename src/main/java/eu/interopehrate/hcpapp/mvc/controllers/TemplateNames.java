68cb44bd402a33559904d4f9a0d1fc4947ba2f19
Nicu Jalba <nicu.jalba@siveco.ro>
2019-08-05 16:02:45 +0300

    Administration as main menu entry

--- src/main/java/eu/interopehrate/hcpapp/mvc/controllers/TemplateNames.java
+++ src/main/java/eu/interopehrate/hcpapp/mvc/controllers/TemplateNames.java
@@ -2,5 +2,5 @@ package eu.interopehrate.hcpapp.mvc.controllers;
 
 public interface TemplateNames {
     String INDEX_TEMPLATE = "index";
-    String HEALTH_CARE_ORGANIZATION_VIEW_DETAILS_TEMPLATE = "health-care-organization/view-details";
+    String HEALTH_CARE_ORGANIZATION_VIEW_DETAILS_TEMPLATE = "administration/health-care-organization/view-details";
 }
--- src/main/java/eu/interopehrate/hcpapp/mvc/controllers/administration/HealthCareOrganizationController.java
+++ src/main/java/eu/interopehrate/hcpapp/mvc/controllers/administration/HealthCareOrganizationController.java
@@ -1,5 +1,6 @@
-package eu.interopehrate.hcpapp.mvc.controllers;
+package eu.interopehrate.hcpapp.mvc.controllers.administration;
 
+import eu.interopehrate.hcpapp.mvc.controllers.TemplateNames;
 import eu.interopehrate.hcpapp.services.HealthCareOrganizationService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import eu.interopehrate.hcpapp.mvc.commands.HealthCareOrganizationCommand;
 
 @Controller
-@RequestMapping("/health-care-organization")
+@RequestMapping("/administration/health-care-organization")
 public class HealthCareOrganizationController {
     private HealthCareOrganizationService healthCareOrganizationService;
 
@@ -28,6 +29,6 @@ public class HealthCareOrganizationController {
     @RequestMapping("/send-info-sher")
     public String sendInformationToSHER(){
         healthCareOrganizationService.sendInformationToSHER();
-        return "redirect:/health-care-organization/view-details";
+        return "redirect:/administration/health-care-organization/view-details";
     }
 }
