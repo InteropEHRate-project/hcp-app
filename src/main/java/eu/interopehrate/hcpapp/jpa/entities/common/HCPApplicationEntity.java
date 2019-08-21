519e8d43562a88dec583abff312f9c681380328b
Nicu Jalba <nicu.jalba@siveco.ro>
2019-08-21 11:05:45 +0300

    common attributes - createdDate, updatedDate

--- src/main/java/eu/interopehrate/hcpapp/jpa/entities/common/HCPApplicationEntity.java
+++ src/main/java/eu/interopehrate/hcpapp/jpa/entities/common/HCPApplicationEntity.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -15,4 +16,20 @@ public class HCPApplicationEntity {
     private Long id;
     @Version
     private Long version;
+    @Column(name = "CREATED_DATE", nullable = false, columnDefinition = "TIMESTAMP")
+    private LocalDateTime createdDate;
+    @Column(name = "UPDATED_DATE", nullable = false, columnDefinition = "TIMESTAMP")
+    private LocalDateTime updatedDate;
+
+    @PrePersist
+    protected void prePersist() {
+        LocalDateTime now = LocalDateTime.now();
+        this.createdDate = now;
+        this.updatedDate = now;
+    }
+
+    @PreUpdate
+    protected void preUpdate() {
+        this.updatedDate = LocalDateTime.now();
+    }
 }
