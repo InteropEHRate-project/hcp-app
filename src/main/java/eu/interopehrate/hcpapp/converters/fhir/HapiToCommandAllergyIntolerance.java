b8555556227251035f15d35ee9c77cc3e1b35442
Nicu Jalba <nicu.jalba@siveco.ro>
2019-09-23 13:37:40 +0300

    the rest of the values

--- src/main/java/eu/interopehrate/hcpapp/converters/fhir/HapiToCommandAllergyIntolerance.java
+++ src/main/java/eu/interopehrate/hcpapp/converters/fhir/HapiToCommandAllergyIntolerance.java
@@ -2,10 +2,14 @@ package eu.interopehrate.hcpapp.converters.fhir;
 
 import eu.interopehrate.hcpapp.mvc.commands.currentpatient.AllergyIntoleranceInfoCommand;
 import org.hl7.fhir.r4.model.AllergyIntolerance;
+import org.hl7.fhir.r4.model.Coding;
+import org.hl7.fhir.r4.model.Identifier;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Component
 public class HapiToCommandAllergyIntolerance implements Converter<AllergyIntolerance, AllergyIntoleranceInfoCommand> {
@@ -15,6 +19,34 @@ public class HapiToCommandAllergyIntolerance implements Converter<AllergyIntoler
         AllergyIntoleranceInfoCommand command = new AllergyIntoleranceInfoCommand();
         if (Objects.nonNull(allergyIntolerance.getCriticality())) {
             command.setCriticality(allergyIntolerance.getCriticality().getDisplay());
+        }
+        if (Objects.nonNull(allergyIntolerance.getType())) {
+            command.setType(allergyIntolerance.getType().getDisplay());
+        }
+        if (!CollectionUtils.isEmpty(allergyIntolerance.getCategory())) {
+            command.setCategory(allergyIntolerance
+                    .getCategory()
+                    .stream()
+                    .map(aice -> aice.getValue().getDisplay())
+                    .collect(Collectors.joining(", "))
+            );
+        }
+        if (!CollectionUtils.isEmpty(allergyIntolerance.getIdentifier())) {
+            command.setIdentifier(allergyIntolerance
+                    .getIdentifier()
+                    .stream()
+                    .map(Identifier::getValue)
+                    .collect(Collectors.joining(", "))
+            );
+        }
+        if (Objects.nonNull(allergyIntolerance.getCode())) {
+            command.setName(allergyIntolerance
+                    .getCode()
+                    .getCoding()
+                    .stream()
+                    .map(Coding::getDisplay)
+                    .collect(Collectors.joining(", "))
+            );
         }
         return command;
     }
