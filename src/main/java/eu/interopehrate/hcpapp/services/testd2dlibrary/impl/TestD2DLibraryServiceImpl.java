e3b10d0168c7d31be27bfc55dda545081e56a51f
Nicu Jalba <nicu.jalba@siveco.ro>
2019-10-16 15:15:16 +0300

    remove the photo of the practitioner

--- src/main/java/eu/interopehrate/hcpapp/services/testd2dlibrary/impl/TestD2DLibraryServiceImpl.java
+++ src/main/java/eu/interopehrate/hcpapp/services/testd2dlibrary/impl/TestD2DLibraryServiceImpl.java
@@ -11,7 +11,6 @@ import eu.interopehrate.hcpapp.mvc.commands.TestD2DLibraryCommand;
 import eu.interopehrate.hcpapp.services.testd2dlibrary.TestD2DLibraryService;
 import org.hl7.fhir.r4.model.*;
 import org.springframework.stereotype.Service;
-import org.springframework.util.MimeTypeUtils;
 
 import java.time.ZoneId;
 import java.util.Collections;
@@ -90,12 +89,8 @@ public class TestD2DLibraryServiceImpl implements TestD2DLibraryService {
         Practitioner.PractitionerQualificationComponent qualification = new Practitioner.PractitionerQualificationComponent();
         qualification.setCode(this.buildQualificationCode(healthCareProfessionalEntity.getOccupation()));
         qualification.setIssuerTarget(organization());
-        Attachment photo = new Attachment()
-                .setContentType(MimeTypeUtils.IMAGE_PNG_VALUE)
-                .setData(healthCareProfessionalEntity.getPicture());
 
         return new Practitioner()
-                .setPhoto(Collections.singletonList(photo))
                 .setQualification(Collections.singletonList(qualification))
                 .setBirthDate(birthDate)
                 .setName(Collections.singletonList(humanName))
