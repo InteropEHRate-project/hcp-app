68552643699d9afe9003b6c0b892c391579ae744
Nicu Jalba <nicu.jalba@siveco.ro>
2020-01-07 11:34:49 +0200

    integrate the library terminal-ihs-hr-exchange

--- src/main/java/eu/interopehrate/hcpapp/HcpWebApplication.java
+++ src/main/java/eu/interopehrate/hcpapp/HcpWebApplication.java
@@ -2,10 +2,11 @@ package eu.interopehrate.hcpapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"eu.interopehrate.ihs.terminalclient", "eu.interopehrate.hcpapp"})
 public class HcpWebApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(HcpWebApplication.class, args);
     }
--- src/main/java/eu/interopehrate/hcpapp/currentsession/CurrentPatient.java
+++ src/main/java/eu/interopehrate/hcpapp/currentsession/CurrentPatient.java
@@ -1,23 +1,40 @@
 package eu.interopehrate.hcpapp.currentsession;
 
 import ca.uhn.fhir.context.FhirContext;
+import eu.interopehrate.ihs.terminalclient.services.TranslateService;
 import org.hl7.fhir.r4.model.AllergyIntolerance;
 import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.Observation;
 import org.hl7.fhir.r4.model.Patient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 import java.util.Objects;
 
 @Component
 public class CurrentPatient {
-    private Bundle patientSummaryBundle;
+    private static final Logger logger = LoggerFactory.getLogger(CurrentPatient.class);
+    private final TranslateService translateService;
+    private Bundle initialPatientSummaryBundle;
+    private Bundle translatedPatientSummaryBundle;
     private Patient patient;
 
+    public CurrentPatient(TranslateService translateService) {
+        this.translateService = translateService;
+    }
+
     public void intiFromJsonString(String patientSummaryJson) {
-        patientSummaryBundle = (Bundle) FhirContext.forR4().newJsonParser().parseResource(patientSummaryJson);
+        initialPatientSummaryBundle = (Bundle) FhirContext.forR4().newJsonParser().parseResource(patientSummaryJson);
+        try {
+            translatedPatientSummaryBundle = translateService.translate(initialPatientSummaryBundle, Locale.UK);
+        } catch (Exception e) {
+            logger.error("Error calling translation service.", e);
+            translatedPatientSummaryBundle = initialPatientSummaryBundle;
+        }
     }
 
     public void initPatient(Patient patient) {
@@ -25,22 +42,22 @@ public class CurrentPatient {
     }
 
     public void reset() {
-        patientSummaryBundle = null;
+        translatedPatientSummaryBundle = null;
     }
 
     public List<AllergyIntolerance> allergyIntoleranceList() {
-        if (Objects.isNull(patientSummaryBundle)) {
+        if (Objects.isNull(translatedPatientSummaryBundle)) {
             return Collections.emptyList();
         } else {
-            return new BundleProcessor(patientSummaryBundle).allergyIntoleranceList();
+            return new BundleProcessor(translatedPatientSummaryBundle).allergyIntoleranceList();
         }
     }
 
-    public List<Observation> observationList(){
-        if (Objects.isNull(patientSummaryBundle)){
+    public List<Observation> observationList() {
+        if (Objects.isNull(translatedPatientSummaryBundle)) {
             return Collections.emptyList();
-        }else{
-            return new BundleProcessor(patientSummaryBundle).observationList();
+        } else {
+            return new BundleProcessor(translatedPatientSummaryBundle).observationList();
         }
     }
 
