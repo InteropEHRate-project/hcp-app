8d3c26f62dbbb33143cef421da2c04be4df9749e
Nicu Jalba <nicu.jalba@siveco.ro>
2020-01-26 21:46:24 +0200

    test with hasData

--- src/main/java/eu/interopehrate/hcpapp/mvc/commands/IndexPatientDataCommand.java
+++ src/main/java/eu/interopehrate/hcpapp/mvc/commands/IndexPatientDataCommand.java
@@ -3,6 +3,8 @@ package eu.interopehrate.hcpapp.mvc.commands;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Objects;
+
 @Getter
 @Setter
 public class IndexPatientDataCommand {
@@ -10,4 +12,8 @@ public class IndexPatientDataCommand {
     private String lastName;
     private String firstName;
     private String consent;
+
+    public Boolean hasData() {
+        return Objects.nonNull(id) || Objects.nonNull(lastName) || Objects.nonNull(firstName) || Objects.nonNull(consent);
+    }
 }
