67d459602838a015bc127fd04bd098d8ec58c872
Andrei Oa <lord_ando10@yahoo.com>
2020-01-28 18:45:06 +0200

    Added Admission Data Audit service test

---
+++ src/test/java/eu/interopehrate/hcpapp/services/administration/AdmissionDataAuditServiceTests.java
@@ -0,0 +1,40 @@
+package eu.interopehrate.hcpapp.services.administration;
+
+import eu.interopehrate.hcpapp.jpa.entities.AdmissionDataAuditEntity;
+import eu.interopehrate.hcpapp.jpa.entities.enums.AuditEventType;
+import eu.interopehrate.hcpapp.jpa.repositories.AdmissionDataAuditRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdmissionDataAuditServiceTests {
+    @Autowired
+    private AdmissionDataAuditRepository admissionDataAuditRepository;
+
+    @Test
+    public void testSaveAudit() {
+        AdmissionDataAuditEntity admissionDataAuditEntity = new AdmissionDataAuditEntity();
+        admissionDataAuditEntity.setDateTime(LocalDateTime.now());
+        admissionDataAuditEntity.setType(AuditEventType.ADMISSION_DATA);
+        admissionDataAuditEntity.setPatientName("patientName");
+        admissionDataAuditEntity.setPatientId("patientId");
+        admissionDataAuditEntity.setDateOfBirth(LocalDate.now());
+        admissionDataAuditEntity.setGender("genderPatient");
+        admissionDataAuditEntity.setHcpName("hcpName");
+        admissionDataAuditEntity.setHcpId("idHCP");
+        admissionDataAuditEntity.setHospitalName("hospitalName");
+        admissionDataAuditEntity.setHospitalId("hospitalId");
+        admissionDataAuditEntity.setDetails(admissionDataAuditEntity.toString());
+        admissionDataAuditEntity = admissionDataAuditRepository.save(admissionDataAuditEntity);
+        assertNotNull(admissionDataAuditEntity.getId());
+    }
+}
