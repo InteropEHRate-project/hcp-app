0b5b2ce7485e4d4fac19012f06f841d47fdcd62d
Nicu Jalba <nicu.jalba@siveco.ro>
2020-02-05 19:30:20 +0200

    sort

--- src/main/java/eu/interopehrate/hcpapp/mvc/controllers/TemplateNames.java
+++ src/main/java/eu/interopehrate/hcpapp/mvc/controllers/TemplateNames.java
@@ -15,15 +15,16 @@ public interface TemplateNames {
     String CURRENT_PATIENT_ALLERGIES_INTOLERANCES_VIEW_SECTION = "current-patient/allergies-intolerances/view-section";
     String CURRENT_PATIENT_ALLERGIES_INTOLERANCES_ADD_PAGE="current-patient/allergies-intolerances/add-info";
     String CURRENT_PATIENT_PROBLEMS_VIEW_SECTION = "current-patient/problems/view-section";
-    String CURRENT_PATIENT_HISTORY_PROCEDURE_VIEW_SECTION = "current-patient/history-procedure/view-section";
     String CURRENT_PATIENT_IMMUNIZATIONS_VIEW_SECTION = "current-patient/immunization/view-section";
+    String CURRENT_PATIENT_HISTORY_PROCEDURE_VIEW_SECTION = "current-patient/history-procedure/view-section";
     String CURRENT_PATIENT_MEDICAL_DEVICES_VIEW_SECTION = "current-patient/medical-devices/view-section";
-    String CURRENT_PATIENT_HISTORY_OF_PAST_ILLNESS_VIEW_SECTION = "current-patient/history-past-illness/view-section";
-    String CURRENT_PATIENT_FUNCTIONAL_STATUS_VIEW_SECTION = "current-patient/functional-status/view-section";
-    String CURRENT_PATIENT_PLAN_OF_CARE_VIEW_SECTION = "current-patient/plan-care/view-section";
-    String CURRENT_PATIENT_SOCIAL_HISTORY_VIEW_SECTION = "current-patient/social-history/view-section";
-    String CURRENT_PATIENT_HISTORY_PREGNANCY_VIEW_SECTION = "current-patient/history-pregnancy/view-section";
-    String CURRENT_PATIENT_ADVANCE_DIRECTIVES_VIEW_SECTION = "current-patient/advance-directives/view-section";
     String CURRENT_PATIENT_DIAGNOSTIC_RESULT_VIEW_SECTION = "current-patient/diagnostic-result/view-section";
     String CURRENT_PATIENT_DIAGNOSTIC_RESULT_ADD_PAGE = "current-patient/diagnostic-result/add-info";
+    // Vital signs
+    String CURRENT_PATIENT_HISTORY_OF_PAST_ILLNESS_VIEW_SECTION = "current-patient/history-past-illness/view-section";
+    String CURRENT_PATIENT_HISTORY_PREGNANCY_VIEW_SECTION = "current-patient/history-pregnancy/view-section";
+    String CURRENT_PATIENT_SOCIAL_HISTORY_VIEW_SECTION = "current-patient/social-history/view-section";
+    String CURRENT_PATIENT_PLAN_OF_CARE_VIEW_SECTION = "current-patient/plan-care/view-section";
+    String CURRENT_PATIENT_FUNCTIONAL_STATUS_VIEW_SECTION = "current-patient/functional-status/view-section";
+    String CURRENT_PATIENT_ADVANCE_DIRECTIVES_VIEW_SECTION = "current-patient/advance-directives/view-section";
 }
