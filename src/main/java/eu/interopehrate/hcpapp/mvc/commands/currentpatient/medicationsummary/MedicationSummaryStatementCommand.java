819962845f6d2b0df1038c9717e37fbdf8cbae88
Nicu Jalba <nicu.jalba@siveco.ro>
2020-02-09 16:09:22 +0200

    form for adding medication statement

--- src/main/java/eu/interopehrate/hcpapp/mvc/commands/currentpatient/medicationsummary/MedicationSummaryStatementCommand.java
+++ src/main/java/eu/interopehrate/hcpapp/mvc/commands/currentpatient/medicationsummary/MedicationSummaryStatementCommand.java
@@ -2,14 +2,25 @@ package eu.interopehrate.hcpapp.mvc.commands.currentpatient.medicationsummary;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Getter
 @Setter
 public class MedicationSummaryStatementCommand {
+    @NotNull
+    @NotEmpty
     private String medication;
+    @NotNull
+    @NotEmpty
     private String status;
+    @NotNull
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
     private LocalDate effective;
+    @NotNull
+    @NotEmpty
     private String patientInstructions;
 }
