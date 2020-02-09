71c9ea5e6e12cb2bccb37f2c06d0bf95881fbb22
Nicu Jalba <nicu.jalba@siveco.ro>
2020-02-09 11:46:07 +0200

    page for add medication statement

--- src/main/java/eu/interopehrate/hcpapp/mvc/controllers/TemplateNames.java
+++ src/main/java/eu/interopehrate/hcpapp/mvc/controllers/TemplateNames.java
@@ -12,6 +12,7 @@ public interface TemplateNames {
     // Current Patient
     String CURRENT_PATIENT_MEDICATION_SUMMARY_VIEW_SECTION = "current-patient/medication-summary/view-section";
     String CURRENT_PATIENT_MEDICATION_SUMMARY_STATEMENT_VIEW_SECTION = "current-patient/medication-summary/statement/view-section";
+    String CURRENT_PATIENT_MEDICATION_SUMMARY_STATEMENT_ADD_PAGE = "current-patient/medication-summary/statement/add-info";
     String CURRENT_PATIENT_MEDICATION_SUMMARY_MEDICATION_VIEW_SECTION = "current-patient/medication-summary/medication/view-section";
     String CURRENT_PATIENT_MEDICATION_SUMMARY_ADD_PAGE = "current-patient/medication-summary/add-info";
     String CURRENT_PATIENT_ALLERGIES_INTOLERANCES_VIEW_SECTION = "current-patient/allergies-intolerances/view-section";
--- src/main/java/eu/interopehrate/hcpapp/mvc/controllers/currentpatient/medicationsummary/MedicationSummaryStatementController.java
+++ src/main/java/eu/interopehrate/hcpapp/mvc/controllers/currentpatient/medicationsummary/MedicationSummaryStatementController.java
@@ -1,11 +1,17 @@
 package eu.interopehrate.hcpapp.mvc.controllers.currentpatient.medicationsummary;
 
+import eu.interopehrate.hcpapp.mvc.commands.currentpatient.medicationsummary.MedicationSummaryStatementCommand;
 import eu.interopehrate.hcpapp.mvc.controllers.TemplateNames;
 import eu.interopehrate.hcpapp.services.currentpatient.medicationsummary.MedicationSummaryStatementService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/current-patient/medication-summary/statement")
@@ -21,5 +27,22 @@ public class MedicationSummaryStatementController {
     public String viewSection(Model model) {
         model.addAttribute("statementCommand", medicationSummaryStatementService.statementCommand());
         return TemplateNames.CURRENT_PATIENT_MEDICATION_SUMMARY_STATEMENT_VIEW_SECTION;
+    }
+
+    @GetMapping
+    @RequestMapping("/open-add-page")
+    public String openAddPage(Model model) {
+        model.addAttribute("medicationSummaryStatementCommand", new MedicationSummaryStatementCommand());
+        return TemplateNames.CURRENT_PATIENT_MEDICATION_SUMMARY_STATEMENT_ADD_PAGE;
+    }
+
+    @PostMapping
+    @RequestMapping("/save-add")
+    public String saveAdd(@Valid @ModelAttribute MedicationSummaryStatementCommand medicationSummaryStatementCommand, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return TemplateNames.CURRENT_PATIENT_MEDICATION_SUMMARY_STATEMENT_ADD_PAGE;
+        }
+        //medicationSummaryService.insertMedicationSummary(medicationSummaryInfoCommand);
+        return "redirect:/current-patient/medication-summary/statement/view-section";
     }
 }
