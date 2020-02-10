7c036a48a5939a1c213f53e60615e48d603d092b
mihaise <mihai.seceleanu@siveco.ro>
2020-02-10 15:32:10 +0200

    optimizations for DiagnosticResults

--- src/main/java/eu/interopehrate/hcpapp/services/currentpatient/DiagnosticResultService.java
+++ src/main/java/eu/interopehrate/hcpapp/services/currentpatient/DiagnosticResultService.java
@@ -1,12 +1,10 @@
 package eu.interopehrate.hcpapp.services.currentpatient;
 
+import eu.interopehrate.hcpapp.mvc.commands.currentpatient.DiagnosticResultCommand;
 import eu.interopehrate.hcpapp.mvc.commands.currentpatient.DiagnosticResultInfoCommand;
-import org.hl7.fhir.r4.model.Observation;
-
-import java.util.List;
 
 public interface DiagnosticResultService {
-    List<DiagnosticResultInfoCommand> diagnosticResultSection();
+    DiagnosticResultCommand diagnosticResultCommand();
 
     void insertDiagnosticResult(DiagnosticResultInfoCommand diagnosticResultInfoCommand);
 }
