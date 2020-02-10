0094520e003f691acfdfe4660981e9304bce14e7
Nicu Jalba <nicu.jalba@siveco.ro>
2020-02-10 16:15:56 +0200

    converter

--- src/main/java/eu/interopehrate/hcpapp/converters/fhir/HapiToCommandProblem.java
+++ src/main/java/eu/interopehrate/hcpapp/converters/fhir/HapiToCommandProblem.java
@@ -3,11 +3,9 @@ package eu.interopehrate.hcpapp.converters.fhir;
 import eu.interopehrate.hcpapp.mvc.commands.currentpatient.ProblemInfoCommand;
 import org.hl7.fhir.r4.model.Coding;
 import org.hl7.fhir.r4.model.Condition;
-
 import org.hl7.fhir.r4.model.DateTimeType;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
-import org.springframework.util.CollectionUtils;
 
 import java.time.ZoneId;
 import java.util.Date;
@@ -21,30 +19,49 @@ public class HapiToCommandProblem implements Converter<Condition, ProblemInfoCom
     public ProblemInfoCommand convert(Condition condition) {
         ProblemInfoCommand problemInfoCommand = new ProblemInfoCommand();
 
-        if(!CollectionUtils.isEmpty(condition.getCode().getCoding())){
-            problemInfoCommand.setName(condition
-                    .getCode()
-                    .getCoding()
-                    .stream()
-                    .map(Coding::getDisplay)
-                    .collect(Collectors.joining(",")));
-            problemInfoCommand.setCode(condition
-                    .getCode()
+        if (Objects.nonNull(condition.getCode())) {
+            problemInfoCommand.setCode(condition.getCode()
                     .getCoding()
                     .stream()
                     .map(Coding::getCode)
-                    .collect(Collectors.joining(",")));
+                    .collect(Collectors.joining("; ")));
+            problemInfoCommand.setName(condition.getCode()
+                    .getCoding()
+                    .stream()
+                    .map(Coding::getDisplay)
+                    .collect(Collectors.joining("; ")));
         }
-
-//        if(Objects.nonNull(condition.getId())){
-//            problemsInfoCommand.setIdProblem(condition.getIdBase());
-//        }
-
-        if (Objects.nonNull(condition.getOnsetDateTimeType())) {
-            Date onsetDateTime = ((DateTimeType)condition.getOnsetDateTimeType()).getValue();
-            problemInfoCommand.setOnSet(onsetDateTime.toInstant()
+        if (Objects.nonNull(condition.getOnset())) {
+            Date onSet = ((DateTimeType) condition.getOnset()).getValue();
+            problemInfoCommand.setOnSet(onSet.toInstant()
                     .atZone(ZoneId.systemDefault())
                     .toLocalDate());
+        }
+        if (Objects.nonNull(condition.getCategory())) {
+            problemInfoCommand.setCategoryCode(condition.getCategory()
+                    .stream()
+                    .flatMap(codeableConcept -> codeableConcept.getCoding().stream())
+                    .map(Coding::getCode)
+                    .collect(Collectors.joining("; ")));
+            problemInfoCommand.setCategoryName(condition.getCategory()
+                    .stream()
+                    .flatMap(codeableConcept -> codeableConcept.getCoding().stream())
+                    .map(Coding::getDisplay)
+                    .collect(Collectors.joining("; ")));
+        }
+        if (Objects.nonNull(condition.getClinicalStatus())) {
+            problemInfoCommand.setClinicalStatus(condition.getClinicalStatus()
+                    .getCoding()
+                    .stream()
+                    .map(Coding::getCode)
+                    .collect(Collectors.joining("; ")));
+        }
+        if (Objects.nonNull(condition.getVerificationStatus())) {
+            problemInfoCommand.setVerificationStatus(condition.getVerificationStatus()
+                    .getCoding()
+                    .stream()
+                    .map(Coding::getCode)
+                    .collect(Collectors.joining(" ")));
         }
 
         return problemInfoCommand;
