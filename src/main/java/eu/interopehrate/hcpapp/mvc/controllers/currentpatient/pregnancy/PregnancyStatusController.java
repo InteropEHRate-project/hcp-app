53308dea6be6d7b7fd3d0f8fc8ad19420c6820ce
mihaise <mihai.seceleanu@siveco.ro>
2020-02-11 16:25:23 +0200

    added tabs for Pregnancy page.

--- src/main/java/eu/interopehrate/hcpapp/mvc/controllers/currentpatient/pregnancy/PregnancyStatusController.java
+++ src/main/java/eu/interopehrate/hcpapp/mvc/controllers/currentpatient/pregnancy/PregnancyStatusController.java
@@ -1,15 +1,16 @@
-package eu.interopehrate.hcpapp.mvc.controllers.currentpatient;
+package eu.interopehrate.hcpapp.mvc.controllers.currentpatient.pregnancy;
 
 import eu.interopehrate.hcpapp.mvc.controllers.TemplateNames;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
-@RequestMapping("/current-patient/pregnancy")
-public class PregnancyController {
+@RequestMapping("/current-patient/pregnancy/status")
+public class PregnancyStatusController {
     @GetMapping
     @RequestMapping("/view-section")
     public String viewSection() {
-        return TemplateNames.CURRENT_PATIENT_PREGNANCY_VIEW_SECTION;
+        return TemplateNames.CURRENT_PATIENT_PREGNANCY_VIEW_SECTION_STATUS;
     }
 }
