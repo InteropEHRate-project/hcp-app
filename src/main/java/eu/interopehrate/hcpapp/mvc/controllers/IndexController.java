8d6c3bc314e94e68d7ae5206cb0b3bd311816faa
mihaise <mihai.seceleanu@siveco.ro>
2020-06-05 14:22:15 +0300

    implemented data about the patient on the NAVBAR.

--- src/main/java/eu/interopehrate/hcpapp/mvc/controllers/IndexController.java
+++ src/main/java/eu/interopehrate/hcpapp/mvc/controllers/IndexController.java
@@ -1,11 +1,15 @@
 package eu.interopehrate.hcpapp.mvc.controllers;
 
 import eu.interopehrate.hcpapp.services.index.IndexService;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
+@Scope("session")
 public class IndexController {
     private IndexService indexService;
 
@@ -14,8 +18,9 @@ public class IndexController {
     }
 
     @RequestMapping({"/", "/index"})
-    public String indexTemplate(Model model) throws Exception {
+    public String indexTemplate(Model model, HttpSession session) throws Exception {
         model.addAttribute("index", indexService.indexCommand());
+        session.setAttribute("mySessionAttribute", indexService.indexCommand());
         return TemplateNames.INDEX_TEMPLATE;
     }
 
