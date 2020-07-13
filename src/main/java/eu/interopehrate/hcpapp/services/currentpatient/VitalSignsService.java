9b7c51002cf3c6e3e38b6beb363bfd989dc18f28
Andrei Oa <lord_ando10@yahoo.com>
2020-07-13 18:37:51 +0300

    Added Screen for Vital Signs

---
+++ src/main/java/eu/interopehrate/hcpapp/services/currentpatient/VitalSignsService.java
@@ -0,0 +1,7 @@
+package eu.interopehrate.hcpapp.services.currentpatient;
+
+import eu.interopehrate.hcpapp.mvc.commands.currentpatient.diagnosticresults.VitalSignsCommand;
+
+public interface VitalSignsService {
+    VitalSignsCommand vitalSignsCommand ();
+}
