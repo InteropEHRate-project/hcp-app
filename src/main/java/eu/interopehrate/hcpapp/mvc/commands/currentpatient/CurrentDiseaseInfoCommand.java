0d0b82fcb4358e6514de40b4fa7c3a08e20c756d
mihaise <mihai.seceleanu@siveco.ro>
2020-07-15 13:20:24 +0300

    added functionality for adding current medications.

--- src/main/java/eu/interopehrate/hcpapp/mvc/commands/currentpatient/CurrentDiseaseInfoCommand.java
+++ src/main/java/eu/interopehrate/hcpapp/mvc/commands/currentpatient/CurrentDiseaseInfoCommand.java
@@ -11,26 +11,16 @@ import java.time.LocalDate;
 @Getter
 @Setter
 public class CurrentDiseaseInfoCommand {
-    @NotNull
-    @NotEmpty
     private String code;
     @NotEmpty
     @NotNull
     private String disease;
     @NotNull
     @DateTimeFormat(pattern = "dd-MM-yyyy")
-    private LocalDate dateOfDiagnosis;
-    @NotNull
-    @NotEmpty
+    private LocalDate dateOfDiagnosis = LocalDate.now();
     private String categoryCode;
-    @NotNull
-    @NotEmpty
     private String categoryName;
-    @NotNull
-    @NotEmpty
     private String clinicalStatus;
-    @NotNull
-    @NotEmpty
     private String verificationStatus;
     @NotNull
     @NotEmpty
