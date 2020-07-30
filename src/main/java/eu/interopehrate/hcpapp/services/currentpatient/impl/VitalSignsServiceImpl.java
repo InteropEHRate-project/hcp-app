d695df8cb5f91bbacb7e948f1dde1915aac451a9
Simona Alexandru <simona.alexandru@siveco.ro>
2020-07-30 13:47:34 +0300

    add translate label for Vital Sign section

--- src/main/java/eu/interopehrate/hcpapp/services/currentpatient/impl/VitalSignsServiceImpl.java
+++ src/main/java/eu/interopehrate/hcpapp/services/currentpatient/impl/VitalSignsServiceImpl.java
@@ -1,6 +1,5 @@
 package eu.interopehrate.hcpapp.services.currentpatient.impl;
 
-import ch.qos.logback.core.net.SyslogOutputStream;
 import eu.interopehrate.hcpapp.currentsession.CurrentPatient;
 import eu.interopehrate.hcpapp.mvc.commands.currentpatient.diagnosticresults.VitalSignsCommand;
 import eu.interopehrate.hcpapp.mvc.commands.currentpatient.diagnosticresults.VitalSignsInfoCommand;
@@ -18,23 +17,24 @@ public class VitalSignsServiceImpl implements VitalSignsService {
     private CurrentPatient currentPatient;
     private List<VitalSignsInfoCommand> vitalSignsInfoCommandsList = new ArrayList<>();
 
+    public VitalSignsServiceImpl(CurrentPatient currentPatient) {
+        this.currentPatient = currentPatient;
+    }
+
     @Override
     public VitalSignsCommand vitalSignsCommand() {
+
         List<VitalSignsInfoCommand> vitalSignsList = new ArrayList<>();
         VitalSignsInfoCommand test = new VitalSignsInfoCommand();
         test.setAnalysis("Heart Rate");
         test.setCurrentValue(90);
         test.setUnitOfMeasurement("bpm");
-        test.setSample(LocalDateTime.of(LocalDate.of(2020,03,12),LocalTime.of(19,20,02)));
-
-        VitalSignsInfoCommand test2 = new VitalSignsInfoCommand();
-        test2.setAnalysis("Breathing Rate");
-        test2.setCurrentValue(120);
-        test2.setUnitOfMeasurement("b/min");
-        test2.setSample(LocalDateTime.of(LocalDate.of(2020,04,12),LocalTime.of(19,20,02)));
+        test.setSample(LocalDateTime.of(LocalDate.of(2020, 03, 12), LocalTime.of(19, 20, 02)));
 
         vitalSignsList.add(test);
-        vitalSignsList.add(test2);
-        return VitalSignsCommand.builder().vitalSignsInfoCommands(vitalSignsList).build();
+        return VitalSignsCommand.builder()
+                .displayTranslatedVersion(currentPatient.getDisplayTranslatedVersion())
+                .vitalSignsInfoCommands(vitalSignsList)
+                .build();
     }
 }
