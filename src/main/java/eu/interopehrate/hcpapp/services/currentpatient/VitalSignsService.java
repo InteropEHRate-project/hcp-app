9c4d16407a4ba9c0e34c78f0b906d5401336a329
mihaise <mihai.seceleanu@siveco.ro>
2020-08-03 15:25:01 +0300

    created method for sending VitalSigns through D2D.

--- src/main/java/eu/interopehrate/hcpapp/services/currentpatient/VitalSignsService.java
+++ src/main/java/eu/interopehrate/hcpapp/services/currentpatient/VitalSignsService.java
@@ -1,7 +1,12 @@
 package eu.interopehrate.hcpapp.services.currentpatient;
 
 import eu.interopehrate.hcpapp.mvc.commands.currentpatient.diagnosticresults.VitalSignsCommand;
+import org.hl7.fhir.r4.model.Bundle;
+
+import java.io.IOException;
 
 public interface VitalSignsService {
     VitalSignsCommand vitalSignsCommand ();
+
+    void sendVitalSigns(Bundle vitalSigns) throws IOException;
 }
--- src/main/java/eu/interopehrate/hcpapp/services/currentpatient/impl/VitalSignsServiceImpl.java
+++ src/main/java/eu/interopehrate/hcpapp/services/currentpatient/impl/VitalSignsServiceImpl.java
@@ -1,24 +1,31 @@
 package eu.interopehrate.hcpapp.services.currentpatient.impl;
 
+import eu.interopehrate.hcpapp.currentsession.CurrentD2DConnection;
 import eu.interopehrate.hcpapp.currentsession.CurrentPatient;
 import eu.interopehrate.hcpapp.mvc.commands.currentpatient.diagnosticresults.VitalSignsCommand;
 import eu.interopehrate.hcpapp.mvc.commands.currentpatient.diagnosticresults.VitalSignsInfoCommand;
 import eu.interopehrate.hcpapp.services.currentpatient.VitalSignsService;
+import lombok.extern.slf4j.Slf4j;
+import org.hl7.fhir.r4.model.Bundle;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Service
 public class VitalSignsServiceImpl implements VitalSignsService {
     private CurrentPatient currentPatient;
     private List<VitalSignsInfoCommand> vitalSignsInfoCommandsList = new ArrayList<>();
+    private CurrentD2DConnection currentD2DConnection;
 
-    public VitalSignsServiceImpl(CurrentPatient currentPatient) {
+    public VitalSignsServiceImpl(CurrentPatient currentPatient, CurrentD2DConnection currentD2DConnection) {
         this.currentPatient = currentPatient;
+        this.currentD2DConnection = currentD2DConnection;
     }
 
     @Override
@@ -36,5 +43,11 @@ public class VitalSignsServiceImpl implements VitalSignsService {
                 .displayTranslatedVersion(currentPatient.getDisplayTranslatedVersion())
                 .vitalSignsInfoCommands(vitalSignsList)
                 .build();
+    }
+
+    @Override
+    public void sendVitalSigns(Bundle vitalSigns) throws IOException {
+        this.currentD2DConnection.getConnectedThread().sendVitalSigns(vitalSigns);
+        log.info("VitalSigns sent to S-EHR");
     }
 }
