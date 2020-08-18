653e51c136a547d6642569605c17f860b43f47d6
Simona Alexandru <simona.alexandru@siveco.ro>
2020-08-18 15:10:11 +0300

    add annotation for vital signs repository

--- src/main/java/eu/interopehrate/hcpapp/jpa/repositories/VitalSignsRepository.java
+++ src/main/java/eu/interopehrate/hcpapp/jpa/repositories/VitalSignsRepository.java
@@ -2,6 +2,8 @@ package eu.interopehrate.hcpapp.jpa.repositories;
 
 import eu.interopehrate.hcpapp.jpa.entities.VitalSignsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VitalSignsRepository extends JpaRepository<VitalSignsEntity, Long> {
 }
