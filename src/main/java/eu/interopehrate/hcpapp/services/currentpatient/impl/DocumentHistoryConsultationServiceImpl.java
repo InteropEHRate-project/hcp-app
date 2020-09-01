4d28116b077dfbe7cd96c9ba29f4c87703c1f981
Simona Alexandru <simona.alexandru@siveco.ro>
2020-09-01 12:10:05 +0300

    add hardcoded data for Document History Consultation

--- src/main/java/eu/interopehrate/hcpapp/services/currentpatient/impl/DocumentHistoryConsultationServiceImpl.java
+++ src/main/java/eu/interopehrate/hcpapp/services/currentpatient/impl/DocumentHistoryConsultationServiceImpl.java
@@ -6,6 +6,7 @@ import eu.interopehrate.hcpapp.mvc.commands.currentpatient.historyconsultation.D
 import eu.interopehrate.hcpapp.services.currentpatient.DocumentHistoryConsultationService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,12 +20,16 @@ public class DocumentHistoryConsultationServiceImpl implements DocumentHistoryCo
     public DocumentHistoryConsultationServiceImpl(CurrentPatient currentPatient) {
         this.currentPatient = currentPatient;
 
+        this.documentHistoryConsultationInfoCommand1.setLocalDateTimeHistoryConsultation(LocalDateTime.of(2020, 8, 2, 10, 51));
         this.documentHistoryConsultationInfoCommand1.setSpeciality("Cardiology");
         this.documentHistoryConsultationInfoCommand1.setLocationHospital("Bucharest");
         this.documentHistoryConsultationInfoCommand1.setExam("Visit");
+
+        this.documentHistoryConsultationInfoCommand2.setLocalDateTimeHistoryConsultation(LocalDateTime.of(2020, 6, 5, 10, 31));
         this.documentHistoryConsultationInfoCommand2.setSpeciality("Psychiatry");
         this.documentHistoryConsultationInfoCommand2.setLocationHospital("Timisoara");
         this.documentHistoryConsultationInfoCommand2.setExam("Visit");
+
         this.documentHistoryConsultationInfoCommands.add(this.documentHistoryConsultationInfoCommand1);
         this.documentHistoryConsultationInfoCommands.add(this.documentHistoryConsultationInfoCommand2);
     }
@@ -52,6 +57,4 @@ public class DocumentHistoryConsultationServiceImpl implements DocumentHistoryCo
         }
         return documentHistoryConsultationInfoCommands;
     }
-
-
 }
