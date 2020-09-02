9f501b6e4a75299b3eb8c6e539303f9c9ed9b981
mihaise <mihai.seceleanu@siveco.ro>
2020-09-02 14:09:17 +0300

    just for testing gitlab RUNNER.

--- src/main/java/eu/interopehrate/hcpapp/HcpWebApplication.java
+++ src/main/java/eu/interopehrate/hcpapp/HcpWebApplication.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan({"eu.interopehrate.ihs.terminalclient", "eu.interopehrate.hcpapp"})
 public class HcpWebApplication {
     public static void main(String[] args) {
-        System.out.println("JUST FOR TESTING THE GITLAB RUNNER");
         SpringApplication.run(HcpWebApplication.class, args);
     }
 }
