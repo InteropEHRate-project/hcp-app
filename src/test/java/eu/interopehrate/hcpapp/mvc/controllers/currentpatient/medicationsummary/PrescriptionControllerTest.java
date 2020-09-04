a1eb906c2f49573c91e8c915573c8965e8daa4e9
mihaise <mihai.seceleanu@siveco.ro>
2020-09-04 09:44:22 +0300

    refactored Prescription unit test.

--- src/test/java/eu/interopehrate/hcpapp/mvc/controllers/currentpatient/medicationsummary/PrescriptionControllerTest.java
+++ src/test/java/eu/interopehrate/hcpapp/mvc/controllers/currentpatient/medicationsummary/PrescriptionControllerTest.java
@@ -8,17 +8,11 @@ import eu.interopehrate.hcpapp.mvc.controllers.TemplateNames;
 import eu.interopehrate.hcpapp.mvc.controllers.currentpatient.currentmedications.prescription.PrescriptionController;
 import eu.interopehrate.hcpapp.services.currentpatient.currentmedications.PrescriptionService;
 import eu.interopehrate.hcpapp.services.currentpatient.impl.currentmedications.PrescriptionServiceImpl;
-import eu.interopehrate.ihs.terminalclient.fhir.TerminalFhirContext;
-import eu.interopehrate.ihs.terminalclient.services.ConceptTranslateService;
-import eu.interopehrate.ihs.terminalclient.services.MachineTranslateService;
-import eu.interopehrate.ihs.terminalclient.services.impl.CodesConversionServiceImpl;
-import eu.interopehrate.ihs.terminalclient.services.impl.TranslateServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
-import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
@@ -29,7 +23,7 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
-public class PrescriptionControllerTest {
+class PrescriptionControllerTest {
     @Mock
     private Model model;
     private PrescriptionController controller;
@@ -38,27 +32,20 @@ public class PrescriptionControllerTest {
     @Mock
     private CurrentD2DConnection currentD2DConnection;
     @Mock
-    private MachineTranslateService machineTranslateService;
-    @Mock
-    private ConceptTranslateService conceptTranslateService;
-    @Mock
-    private TerminalFhirContext terminalFhirContext;
-    private String keyword = "";
-    @Mock
     private HttpSession session;
+    @Mock
+    private CurrentPatient currentPatient;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.initMocks(this);
-        PrescriptionService service = new PrescriptionServiceImpl
-                (new CurrentPatient(new TranslateServiceImpl(this.conceptTranslateService, this.machineTranslateService), new CodesConversionServiceImpl(new RestTemplate(), terminalFhirContext), terminalFhirContext),
-                        hapiToCommandPrescription, currentD2DConnection);
+        PrescriptionService service = new PrescriptionServiceImpl(this.currentPatient, this.hapiToCommandPrescription, this.currentD2DConnection);
         this.controller = new PrescriptionController(service);
     }
 
     @Test
     void viewSection() throws IOException {
-        String returnedString = this.controller.viewSection(this.model, this.session, this.keyword);
+        String returnedString = this.controller.viewSection(this.model, this.session, "");
         assertEquals(TemplateNames.CURRENT_PATIENT_CURRENT_MEDICATIONS_PRESCRIPTION_VIEW_SECTION, returnedString);
         verify(this.model, times(1)).addAttribute(eq("prescriptionCommand"), any(PrescriptionCommand.class));
     }
