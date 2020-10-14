477ec5f1ea6846f128859823719c5f1f0bf1607f
mihaise <mihai.seceleanu@siveco.ro>
2020-10-14 13:00:30 +0300

    display PDF file.

--- src/main/java/eu/interopehrate/hcpapp/mvc/controllers/currentpatient/DocumentHistoryConsultationController.java
+++ src/main/java/eu/interopehrate/hcpapp/mvc/controllers/currentpatient/DocumentHistoryConsultationController.java
@@ -1,6 +1,5 @@
 package eu.interopehrate.hcpapp.mvc.controllers.currentpatient;
 
-import eu.interopehrate.hcpapp.mvc.commands.currentpatient.historyconsultation.DocumentHistoryConsultationInfoCommand;
 import eu.interopehrate.hcpapp.mvc.controllers.TemplateNames;
 import eu.interopehrate.hcpapp.services.currentpatient.DocumentHistoryConsultationService;
 import org.springframework.stereotype.Controller;
@@ -8,10 +7,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
 
 @Controller
 @RequestMapping("/current-patient/document-history-consultation")
@@ -29,17 +24,5 @@ public class DocumentHistoryConsultationController {
         model.addAttribute("isFiltered", this.documentHistoryConsultationService.isFiltered());
         model.addAttribute("isEmpty", this.documentHistoryConsultationService.isEmpty());
         return TemplateNames.CURRENT_PATIENT_DOCUMENT_HISTORY_CONSULTATION_VIEW_SECTION;
-    }
-
-    @GetMapping
-    @RequestMapping("/pdf/{speciality}/{exam}/{date}")
-    public OutputStream pdf(@PathVariable(value = "speciality") String speciality,
-                            @PathVariable(value = "exam") String exam,
-                            @PathVariable(value = "date")String date) throws IOException {
-        OutputStream out = new FileOutputStream("documentHistoryConsultation.pdf");
-        DocumentHistoryConsultationInfoCommand doc = this.documentHistoryConsultationService.documentHistoryConsultationCommand(speciality).find(exam, date);
-        out.write(doc.getData());
-        out.close();
-        return out;
     }
 }
