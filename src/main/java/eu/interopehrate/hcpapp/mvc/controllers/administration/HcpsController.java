ab37f7512efb9b09c920735b07e24de73f8e4e00
mihaise <mihai.seceleanu@siveco.ro>
2021-02-03 12:57:34 +0200

    handled exception on Connection timeout.

--- src/main/java/eu/interopehrate/hcpapp/mvc/controllers/administration/HcpsController.java
+++ src/main/java/eu/interopehrate/hcpapp/mvc/controllers/administration/HcpsController.java
@@ -1,15 +1,18 @@
 package eu.interopehrate.hcpapp.mvc.controllers.administration;
 
 import eu.interopehrate.hcpapp.mvc.controllers.TemplateNames;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
+@Slf4j
 @Controller
 @RequestMapping("/administration/hcps")
 public class HcpsController {
@@ -21,7 +24,15 @@ public class HcpsController {
     @GetMapping
     @RequestMapping("/view-details")
     public String detailsTemplate(Model model) {
-        model.addAttribute("hcps", new RestTemplate().getForObject(this.hospitalServicesUrl + this.hcpsListUrl, List.class));
+        boolean error = false;
+        try {
+            model.addAttribute("hcps", new RestTemplate().getForObject(this.hospitalServicesUrl + this.hcpsListUrl, List.class));
+        } catch (ResourceAccessException e) {
+            log.error("Connection refused");
+            error = true;
+        } finally {
+            model.addAttribute("error", error);
+        }
         return TemplateNames.ADMINISTRATION_HCPS_VIEW_DETAILS;
     }
 }
