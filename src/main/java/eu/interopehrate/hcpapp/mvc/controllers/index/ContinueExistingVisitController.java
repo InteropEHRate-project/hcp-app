1c3126fab1fa70ff9d1aaf3047e7ae1eaf3f0f59
Simona Alexandru <simona.alexandru@siveco.ro>
2021-02-12 15:25:41 +0200

    fix the error when connection in the hcps app is not establish

--- src/main/java/eu/interopehrate/hcpapp/mvc/controllers/index/ContinueExistingVisitController.java
+++ src/main/java/eu/interopehrate/hcpapp/mvc/controllers/index/ContinueExistingVisitController.java
@@ -3,16 +3,19 @@ package eu.interopehrate.hcpapp.mvc.controllers.index;
 import eu.interopehrate.hcpapp.jpa.entities.HealthCareProfessionalEntity;
 import eu.interopehrate.hcpapp.jpa.repositories.HealthCareProfessionalRepository;
 import eu.interopehrate.hcpapp.mvc.controllers.TemplateNames;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
+@Slf4j
 @Controller
 @Scope("session")
 @RequestMapping("/index")
@@ -30,8 +33,16 @@ public class ContinueExistingVisitController {
     @GetMapping
     @RequestMapping({"/existing-visit"})
     public String indexTemplate(Model model) {
-        List<HealthCareProfessionalEntity> all = this.healthCareProfessionalRepository.findAll();
-        model.addAttribute("existingVisit", restTemplate.postForObject(this.hospitalServicesUrl + "/patients" + "/list", all.get(0).getId(), List.class));
+        boolean error = false;
+        try {
+            List<HealthCareProfessionalEntity> all = this.healthCareProfessionalRepository.findAll();
+            model.addAttribute("existingVisit", restTemplate.postForObject(this.hospitalServicesUrl + "/patients" + "/list", all.get(0).getId(), List.class));
+        } catch (ResourceAccessException e) {
+            log.error("Connection is not establish");
+            error = true;
+        } finally {
+            model.addAttribute("error", error);
+        }
         return TemplateNames.INDEX_EXISTING_VISIT;
     }
 }
