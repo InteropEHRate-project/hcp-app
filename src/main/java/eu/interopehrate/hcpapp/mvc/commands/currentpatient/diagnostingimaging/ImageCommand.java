a2793c2579876c1e9881fa9f0e4b138a8366ade9
mihaise <mihai.seceleanu@siveco.ro>
2021-02-25 17:27:49 +0200

    added DiagnosticReports documents extraction from ImageReport (part 2) final.

--- src/main/java/eu/interopehrate/hcpapp/mvc/commands/currentpatient/diagnostingimaging/ImageCommand.java
+++ src/main/java/eu/interopehrate/hcpapp/mvc/commands/currentpatient/diagnostingimaging/ImageCommand.java
@@ -10,4 +10,13 @@ import java.util.List;
 public class ImageCommand {
     private List<ImageInfoCommand> imageInfoCommands;
     private List<DiagnosticReportInfoCommand> diagnosticReportInfoCommands;
+
+    public DiagnosticReportInfoCommand find(String nameDiagnostic, String dateOfReport) {
+        for (DiagnosticReportInfoCommand diag : this.diagnosticReportInfoCommands) {
+            if (diag.getNameDiagnostic().equals(nameDiagnostic) && diag.getDateOfReport().toString().equals(dateOfReport)) {
+                return diag;
+            }
+        }
+        return null;
+    }
 }
