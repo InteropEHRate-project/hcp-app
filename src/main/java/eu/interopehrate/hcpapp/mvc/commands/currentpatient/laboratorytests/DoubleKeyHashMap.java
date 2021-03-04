90e617cc968922b154dc0f52a6606372f21a099c
mihaise <mihai.seceleanu@siveco.ro>
2021-03-04 16:41:11 +0200

    optimizations (part 5).

--- src/main/java/eu/interopehrate/hcpapp/mvc/commands/currentpatient/laboratorytests/DoubleKeyHashMap.java
+++ src/main/java/eu/interopehrate/hcpapp/mvc/commands/currentpatient/laboratorytests/DoubleKeyHashMap.java
@@ -6,21 +6,16 @@ import java.util.Set;
 import java.util.function.BiFunction;
 
 public class DoubleKeyHashMap<T1, T2, T3> {
-
-	private HashMap<Pair<T1, T2>, T3> hashMap;
+	private final HashMap<Pair<T1, T2>, T3> hashMap;
 
 	public DoubleKeyHashMap() {
-		hashMap = new HashMap<Pair<T1, T2>, T3>();
+		hashMap = new HashMap<>();
 	}
 
 	@SuppressWarnings("hiding")
-	class Pair<T1, T2> {
+	static class Pair<T1, T2> {
 		T1 key1;
 		T2 key2;
-
-		Pair() {
-			super();
-		}
 
 		Pair(T1 key1, T2 key2) {
 			this.key1 = key1;
@@ -34,7 +29,6 @@ public class DoubleKeyHashMap<T1, T2, T3> {
 
 		@Override
 		public boolean equals(Object obj) {
-
 			if (obj == this)
 				return true;
 			if (!(obj instanceof Pair)) {
@@ -42,36 +36,32 @@ public class DoubleKeyHashMap<T1, T2, T3> {
 			}
 			@SuppressWarnings("unchecked")
 			Pair<T1, T2> pair = (Pair<T1, T2>) obj;
-
-			if (this.key1.equals(pair.key1) && this.key2.equals(pair.key2)) {
-				return true;
-			}
-			return false;
+			return this.key1.equals(pair.key1) && this.key2.equals(pair.key2);
 		}
 	}
 
 	public T3 put(T1 key1, T2 key2, T3 value) {
-		T3 prevVal = null;
-		prevVal = this.hashMap.get(new Pair<T1, T2>(key1, key2));
-		this.hashMap.put(new Pair<T1, T2>(key1, key2), value);
+		T3 prevVal;
+		prevVal = this.hashMap.get(new Pair<>(key1, key2));
+		this.hashMap.put(new Pair<>(key1, key2), value);
 		return prevVal;
 	}
 
 	public T3 get(T1 key1, T2 key2) {
-		T3 val = null;
-		val = this.hashMap.get(new Pair<T1, T2>(key1, key2));
+		T3 val;
+		val = this.hashMap.get(new Pair<>(key1, key2));
 		return val;
 	}
 
 	public T3 remove(T1 key1, T2 key2) {
-		T3 prevVal = null;
-		prevVal = this.hashMap.get(new Pair<T1, T2>(key1, key2));
-		this.hashMap.remove(new Pair<T1, T2>(key1, key2));
+		T3 prevVal;
+		prevVal = this.hashMap.get(new Pair<>(key1, key2));
+		this.hashMap.remove(new Pair<>(key1, key2));
 		return prevVal;
 	}
 
 	public boolean containsKey(T1 key1, T2 key2) {
-		return this.hashMap.containsKey(new Pair<T1, T2>(key1, key2));
+		return this.hashMap.containsKey(new Pair<>(key1, key2));
 	}
 
 	public Set<Pair<T1, T2>> keySet() {
@@ -89,18 +79,18 @@ public class DoubleKeyHashMap<T1, T2, T3> {
 	}
     	
 	public T3 computeIfAbsent(T1 key1, T2 key2, BiFunction<T1,T2,T3> value) {
-        	return this.hashMap.computeIfAbsent(new Pair<>(key1,key2), (k) -> value.apply(key1,key2));
-    	}
-	
+        	return this.hashMap.computeIfAbsent(new Pair<>(key1, key2), (k) -> value.apply(key1,key2));
+	}
+
 	@Override
 	public String toString() {
-		StringBuffer stringBuffer = new StringBuffer("{\n");
+		StringBuilder stringBuffer = new StringBuilder("{\n");
 		Set<Pair<T1, T2>> keySet = this.keySet();
 		Iterator<Pair<T1, T2>> itr = keySet.iterator();
 		while (itr.hasNext()) {
-			Pair<T1, T2> keyPair = (Pair<T1, T2>) itr.next();
-			stringBuffer.append("   [" + keyPair.key1 + ", " + keyPair.key2 + "] = "
-					+ this.hashMap.get(new Pair<T1, T2>(keyPair.key1, keyPair.key2)));
+			Pair<T1, T2> keyPair = itr.next();
+			stringBuffer.append("   [").append(keyPair.key1).append(", ").append(keyPair.key2).append("] = ")
+					.append(this.hashMap.get(new Pair<>(keyPair.key1, keyPair.key2)));
 			if(itr.hasNext()) {
 				stringBuffer.append(",\n");
 			}
@@ -108,5 +98,4 @@ public class DoubleKeyHashMap<T1, T2, T3> {
 		stringBuffer.append("\n}");
 		return stringBuffer.toString();
 	}
-
 }
