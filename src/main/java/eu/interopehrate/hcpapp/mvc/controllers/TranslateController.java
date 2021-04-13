53376713a4165595f6f6fd2dc55b7f1f40a3d7cb
mihaise <mihai.seceleanu@siveco.ro>
2021-04-13 11:50:30 +0300

    integrated Translation in the Internationalization.

---
+++ src/main/java/eu/interopehrate/hcpapp/mvc/controllers/TranslateController.java
@@ -0,0 +1,29 @@
+package eu.interopehrate.hcpapp.mvc.controllers;
+
+import eu.interopehrate.hcpapp.currentsession.CurrentPatient;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class TranslateController {
+    private final CurrentPatient currentPatient;
+
+    public TranslateController(CurrentPatient currentPatient) {
+        this.currentPatient = currentPatient;
+    }
+
+    @GetMapping
+    @RequestMapping("/language")
+    public String english(@RequestParam(name = "lang") String lang, HttpServletRequest httpServletRequest) {
+        if (lang.equals("en")) {
+            currentPatient.setDisplayTranslatedVersion(Boolean.TRUE);
+        } else if (lang.equals("it")) {
+            currentPatient.setDisplayTranslatedVersion(Boolean.FALSE);
+        }
+        return "redirect:" + httpServletRequest.getHeader("referer");
+    }
+}
